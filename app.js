dbb20e284f9916ffa50447c3d7cc7ae8d37e0f1d
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-03-09 12:04:38 +0530

    Add API routes for authentication, users, and subscriptions

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ import express from 'express';
 
 import { PORT } from './config/env.js';
 
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import subscriptionRouter from './routes/subscription.route.js';
+
 const app = express();
+
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/subscriptions', subscriptionRouter)
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Suscription Tracker API!');
